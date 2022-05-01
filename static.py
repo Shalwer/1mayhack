10234cba21a8dd9747febf3c3236ce977db49862
Денис Толпегин <Shalwer@yandex.ru>
2022-05-01 13:03:58 +0300

    add

---
+++ static.py
@@ -0,0 +1,7 @@
+start_text = 'Текст для старта'
+
+
+def default_answer(message):
+    answer = f'Мы можем поболтать, но только как бот с человеком.\n' \
+              f'То есть: \n- {message} \n- Увы, я не знаю этой команды Т_Т '
+    return answer
