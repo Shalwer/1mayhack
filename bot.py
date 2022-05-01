c456c3d223bf25c2483347baa7bc98bb3800b4bd
Денис Толпегин <Shalwer@yandex.ru>
2022-05-01 15:14:21 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-
+import utility
 import config
 import static
 
@@ -11,9 +11,25 @@ def messages_for_start(message):
     bot.send_message(message.chat.id, static.start_text)
 
 
+@bot.message_handler(commands=["maths"])
+def messages_for_start(message):
+    bot.send_message(message.chat.id, static.maths)
+
+
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, static.default_answer(message.text))
+def maths_func(message):
+    split_massage = message.text.split()
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Приветствую тебя!')
+    elif split_massage[0] in utility.all_math_func:
+        if len(split_massage) == 1:
+            bot.send_message(message.chat.id, f'{split_massage[0]} чего?')
+        else:
+            result = utility.math_func(split_massage[0], split_massage[1])
+            bot.send_message(message.chat.id,
+                                 f'{split_massage[0]} = {result}')
+    else:
+        bot.send_message(message.chat.id, static.default_answer(message.text))
 
 
 if __name__ == '__main__':
